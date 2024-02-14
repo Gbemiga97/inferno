023912016e8bdc7a0c2e152b3086351bfd25f61a
Gbemiga Salam <holuwagbemiga97@gmail.com>
2024-02-14 10:22:11 +0100

    second commit

---
+++ src/utils/images.js
@@ -0,0 +1,43 @@
+import AboutImg from '../assets/about/img.png'
+import Brand1 from '../assets/brands/01.svg'
+import Brand2 from '../assets/brands/02.svg'
+import Brand3 from '../assets/brands/03.svg'
+import Brand4 from '../assets/brands/04.svg'
+import Brand5 from '../assets/brands/05.svg'
+import HeroBg from '../assets/hero/bg.jpg'
+import News1 from '../assets/news/01.png'
+import News2 from '../assets/news/02.png'
+import News3 from '../assets/news/03.png'
+import Testimonial1 from '../assets/testimonial/01.png'
+import Testimonial2 from '../assets/testimonial/02.png'
+import Testimonial3 from '../assets/testimonial/03.png'
+import Testimonial4 from '../assets/testimonial/04.png'
+import Work1 from '../assets/work/01.png'
+import Work2 from '../assets/work/02.png'
+import Work3 from '../assets/work/03.png'
+import Work4 from '../assets/work/04.png'
+
+
+
+const images = {
+    AboutImg,
+    Brand1,
+    Brand2,
+    Brand3,
+    Brand4,
+    Brand5,
+    HeroBg,
+    News1,
+    News2,
+    News3,
+    Testimonial1,
+    Testimonial2,
+    Testimonial3,
+    Testimonial4,
+    Work1,
+    Work2,
+    Work3,
+    Work4
+}
+
+
